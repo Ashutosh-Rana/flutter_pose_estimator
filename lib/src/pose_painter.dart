8b8e98107d0793d52b4c5a3823e9470cebf91c4d
Ashutosh Rana <ashutoshrana978@gmail.com>
2023-04-08 01:05:16 +0530

    detection of straight hands done

--- lib/src/pose_painter.dart
+++ lib/src/pose_painter.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:vector_math/vector_math.dart' as p;
 // import 'package:google_ml_kit/google_ml_kit.dart';
 
 import '../google_mlkit_pose_detection.dart';
@@ -60,28 +63,19 @@ class PosePainter extends CustomPainter {
         color: Colors.black,
         fontSize: 30,
       );
-      TextSpan textSpan(String s){
-        return TextSpan(
-          text: s,
-          style: textStyle
-        );
+      TextSpan textSpan(String s) {
+        return TextSpan(text: s, style: textStyle);
       }
-      TextPainter textPaint(PoseLandmarkType type){
+
+      TextPainter textPaint(PoseLandmarkType type) {
         final PoseLandmark joint = pose.landmarks[type]!;
         return TextPainter(
           text: textSpan(joint.likelihood.toString()),
           textDirection: TextDirection.ltr,
         );
       }
-      // final textSpan = TextSpan(
-      //   text: 'Hello, world.',
-      //   style: textStyle,
-      // );
-      // final textPainter = TextPainter(
-      //   text: textSpan,
-      //   textDirection: TextDirection.ltr,
-      // );
-      TextPainter textPainter=textPaint(PoseLandmarkType.rightWrist);
+
+      TextPainter textPainter = textPaint(PoseLandmarkType.rightWrist);
       textPainter.layout(
         minWidth: 0,
         maxWidth: size.width,
@@ -89,7 +83,33 @@ class PosePainter extends CustomPainter {
       final xCenter = (size.width - textPainter.width) / 2;
       final yCenter = (size.height - textPainter.height) / 2;
       final offset = Offset(xCenter, yCenter);
-      textPainter.paint(canvas, offset);
+      // textPainter.paint(canvas, offset);
+
+      double checkStraightHands(PoseLandmarkType firstPoint, PoseLandmarkType midPoint,
+          PoseLandmarkType lastPoint) {
+        final PoseLandmark joint1 = pose.landmarks[firstPoint]!;
+        final PoseLandmark joint2 = pose.landmarks[midPoint]!;
+        final PoseLandmark joint3 = pose.landmarks[lastPoint]!;
+        var result = p.degrees(atan2(joint3.y - joint2.y, joint3.x - joint2.x) -
+            atan2(joint1.y - joint2.y, joint1.x - joint2.x));
+        result = result.abs(); // Angle should never be negative
+        if (result > 180) {
+          result = 360.0 -
+              result; // Always get the acute representation of the angle
+        }
+        
+        return result;
+      }
+      
+      var result1=checkStraightHands(PoseLandmarkType.rightWrist, PoseLandmarkType.rightElbow, PoseLandmarkType.rightShoulder);
+      var result2=checkStraightHands(PoseLandmarkType.leftWrist, PoseLandmarkType.leftElbow, PoseLandmarkType.leftShoulder);
+      var circlePaint;
+        if (result1 >= 170 && result2>=170) {
+          circlePaint = Paint()..color = Colors.green;
+        } else {
+          circlePaint = Paint()..color = Colors.red;
+        }
+        canvas.drawCircle(Offset(20, 20), 10, circlePaint);
 
       //Draw arms
       paintLine(
